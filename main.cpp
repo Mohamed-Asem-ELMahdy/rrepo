2af38aaaeb9defc7da5977577ca77599c6dcd747
Mohamed Asem <m.asemelmahdy@gmail.com>
2024-07-03 19:09:35 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+
+#define moasem ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define test  ll te;cin>>te;while(te--)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define isON(n, k) (((n)>>(k))&1)
+#define vi vector<ll>
+typedef long long ll;
+const ll oo = 1e9;
+const ll N = 1e5 + 3, MOD = 1e9 + 7;
+using namespace std;
+/////////problem///////////////
+
+void solve() {
+
+}
+
+signed main() {
+    moasem;
+    //test {
+    // memset(memo, -1, sizeof memo);
+    solve();
+    cout << endl;
+    // }
+}
